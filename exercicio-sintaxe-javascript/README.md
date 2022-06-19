25f10e43372f7532e6c99fa44bfdab61dd08bab3
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-06-19 01:20:16 -0300

    Parte-1

---
+++ exercicio-sintaxe-javascript/README.md
@@ -0,0 +1,10 @@
+##Atividade JavaScript
+
+- Criar uma função que receba dois números como parâmetros.
+
+- Depois conferir se os números são iguais.
+
+- E se a soma dos números é maior que 10 ou menor que 20.
+
+- No final, retornar uma string informando se os números são iguais ou não.
+E a soma é(...), maior ou menor que 10 e maior ou menor que 20.
---
+++ exercicio-sintaxe-javascript/compararNúmeros.js
@@ -0,0 +1,33 @@
+function comparaNumeros(N1, N2) {
+    const primeiraEtapa = criaEtapa(N1, N2);
+    const segundaEtapa = criaSegundaetapa(N1, N2);
+
+    return `${primeiraEtapa} ${segundaEtapa}`
+}
+
+function criaEtapa(N1, N2) {
+    let saoIguais = '';
+    if(N1 !== N2) {
+        saoIguais = 'não'; 
+    }
+    return `Os números ${N1} e ${N2} $ {saoIguais} são iguais.`
+};
+
+//function criaSegundaetapa(N1, N2) {
+ //   const soma = N1 + N2;
+    
+  //  let resultado10 = 'menor';
+  //  let resultado20 = 'menor';
+  //  const compara10 = soma > 10;
+  //  const compara20 = soma > 20;
+
+   // if(compara10) {
+   //     resultado10 = 'maior';
+   // }
+
+   // if(compara20) {
+    //    resultado20 = 'maior';
+    //}
+
+   // return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`
+//}
