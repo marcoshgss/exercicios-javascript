eb4e93c9e4d212dc49be38220ab043f42b1234d1
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-06-21 10:28:37 -0300

    exercicio 2

---
+++ exercicio-palindromo-javascript/arrayPares.js
@@ -0,0 +1,14 @@
+function substituiPares(array) {
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === 0) {
+            console.log("Você já é zero!");
+        } else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0 ...`);
+            array[i] = 0;
+        }
+    }
+    return array;
+}
+
+let A =[1, 3, 4, 5, 6, 7, 8];
+substituiPares(A)
