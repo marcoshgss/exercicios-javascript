86278558b8c4b09ceb2bf825444db0591934a4cf
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-06-23 09:39:10 -0300

    update-exercicio-funções

--- exercicio-funções/alunosAprovados.js
+++ exercicio-funções/alunosAprovados.js
@@ -1,3 +1,29 @@
-function alunosAprovados (array, media){
+const alunos = [
+    {
+        nome: 'Andre',
+        nota: 5,
+        turma: 'A'
+    },
+    {
+        nome: 'Julia',
+        nota: 9,
+        turma: 'C'
+    },
+    {
+        nome: 'Felipe',
+        nota: 6,
+        turma: 'B'
+    },
+];
 
+function alunosAprovados(array, media) {
+    let aprovados = [];
+    for (let i = 0; i < array.length; i++) {
+       const {nota, nome} = array[i];
+
+        if(array[i].nota >= media) {
+            aprovados.push(array[i].nome);
+        }
+    }
+    return aprovados;
 }
