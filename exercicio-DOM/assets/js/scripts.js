a0f38e047798a0b9b0601326d3b0eb71af490e7f
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-06-25 17:44:05 -0300

    Exercicio- Manipulando-DOM

---
+++ exercicio-DOM/assets/js/scripts.js
@@ -0,0 +1,9 @@
+function changeMode(){
+
+}
+
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body');
+
+button.addEventListener('click', changeMode);
