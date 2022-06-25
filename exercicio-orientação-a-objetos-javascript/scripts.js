9238042f2e4324d3e6e71b1e737d0b10aafc4d31
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-06-25 15:16:06 -0300

    exercicio-orientação-a-objetos

---
+++ exercicio-orientação-a-objetos-javascript/scripts.js
@@ -0,0 +1,39 @@
+class contaBancaria {
+    constructor(agencia, numero, tipo, saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = saldo;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return 'Operação negada';
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar(valor) {
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente {
+    constructor(agencia, numero, saldo, cartaoCredito) {
+        super(agencia, numero, saldo);
+        this.tipo = 'corrente';
+        this.cartaoCredito = cartaoCredito;
+    }
+}
