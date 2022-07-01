449340298eae1c525ecdacf173097eebac37ca66
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-07-01 14:53:59 -0300

    Desafio

---
+++ Desafios-javascript-DIO/Desafio-3-Papalegua/papalegua.js
@@ -0,0 +1,14 @@
+let input = gets()
+
+let qtdEntradas = parseInt(input)
+let melhor = 999
+
+for (let i = 0; i < qtdEntradas; i++) {
+
+    input = gets()
+    let tempo = parseFloat(input)
+    if (tempo < melhor)
+        melhor = tempo
+
+}
+print(melhor)
